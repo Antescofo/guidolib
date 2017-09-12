764460a193e82d9e4fcafde56d9ec2c1f63fdcf1
Arshia Cont <arshia.cont@ircam.fr>
2017-09-12 12:45:47 +0200

    update XCode build bash

--- build/buildXCodeProject.sh
+++ build/buildXCodeProject.sh
@@ -10,3 +10,14 @@ if [ ! -d ./ios/guidolib.xcodeproj  ]; then
 else
   echo "./guidolib/build/ios/guidolib.xcodeproj already  exists"
 fi
+
+if [ ! -d ./macos/guidolib.xcodeproj  ]; then
+if [ ! -d macos  ]; then
+mkdir macos
+fi
+cd ./macos
+cmake .. -DMIDIEXPORT=no -DIOS=no -G Xcode
+cd ..
+else
+echo "./guidolib/build/ios/guidolib.xcodeproj already  exists"
+fi
