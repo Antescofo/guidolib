b79fe0c0f38442a55142221bfb9c45a92d97052e
Victor Lenoir <victorlenoir@antescofo.com>
2019-09-02 14:50:48 +0200

    add outro

--- tools/videogen.sh
+++ tools/videogen.sh
@@ -11,5 +11,5 @@ videogeneration $1 $2 && \
     sox -t f32 -r 48000 -c 1 audio.raw audio.wav && \
     ffmpeg -i audio.wav -i $3 -filter_complex amix=inputs=2:duration=longest final_audio.mp3 && \
     ffmpeg -i output_watermarked.mp4 -i final_audio.mp3 -c copy almost_done.mp4 && \
-    ffmpeg -i intro.mp4 -i almost_done.mp4 -filter_complex "[0:v] [0:a] [1:v] [1:a] concat=n=2:v=1:a=1 [v] [a]" -map "[v]" -map "[a]" -strict -2 merged.mp4 && \
+    ffmpeg -i intro.mp4 -i almost_done.mp4 -i outro.mp4 -filter_complex "[0:v] [0:a] [1:v] [1:a] [2:v] [2:a] concat=n=3:v=1:a=1 [v] [a]" -map "[v]" -map "[a]" -strict -2 merged.mp4 && \
     cp merged.mp4 $4
