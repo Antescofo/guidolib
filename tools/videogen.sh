1ad7f21ed5e2307eb52cea55fd8c6e19d3efa921
Victor Lenoir <victorlenoir@antescofo.com>
2019-09-02 12:08:40 +0200

    videogen: add intro by gwenolé

--- tools/videogen.sh
+++ tools/videogen.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-rm -f output* $4 final_audio.mp3 audio.raw audio.wav
+rm -f output* $4 final_audio.mp3 audio.raw audio.wav merged.mp4 almost_done.mp4
 
 # audioselector $3
 # exit 1
@@ -10,6 +10,6 @@ videogeneration $1 $2 && \
     rm -f output*.png && \
     sox -t f32 -r 48000 -c 1 audio.raw audio.wav && \
     ffmpeg -i audio.wav -i $3 -filter_complex amix=inputs=2:duration=longest final_audio.mp3 && \
-    ffmpeg -i output_watermarked.mp4 -i final_audio.mp3 -c copy $4
-
-# ffmpeg -i tools/intro.mkv -i tools/final_output.mp4 -filter_complex "[0:v] [0:a] [1:v] [1:a] concat=n=2:v=1:a=1 [v] [a]" -map "[v]" -map "[a]" out.mp4
+    ffmpeg -i output_watermarked.mp4 -i final_audio.mp3 -c copy almost_done.mp4 && \
+    ffmpeg -i intro.mp4 -i almost_done.mp4 -filter_complex "[0:v] [0:a] [1:v] [1:a] concat=n=2:v=1:a=1 [v] [a]" -map "[v]" -map "[a]" -strict -2 merged.mp4 && \
+    cp merged.mp4 $4
