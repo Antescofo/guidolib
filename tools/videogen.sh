1625db7d42e450782782def235e0fbaf74088ea9
Victor Lenoir <victorlenoir@antescofo.com>
2019-09-02 15:39:16 +0200

    add logs

--- tools/videogen.sh
+++ tools/videogen.sh
@@ -1,15 +1,13 @@
 #! /bin/bash
 
-rm -f output* $4 final_audio.mp3 audio.raw audio.wav merged.mp4 almost_done.mp4
+rm -f output* $4 *.raw *.wav merged.mp4 almost_done.mp4 final_audio.mp3
 
-# audioselector $3
-# exit 1
-videogeneration $1 $2 && \
-    ffmpeg -framerate 24 -i output%01d.png output.mp4 && \
-    ffmpeg -i output.mp4 -i watermark_10a.png -filter_complex "overlay=0:0" output_watermarked.mp4 && \
-    rm -f output*.png && \
-    sox -t f32 -r 48000 -c 1 audio.raw audio.wav && \
-    ffmpeg -i audio.wav -i $3 -filter_complex amix=inputs=2:duration=longest final_audio.mp3 && \
-    ffmpeg -i output_watermarked.mp4 -i final_audio.mp3 -c copy almost_done.mp4 && \
-    ffmpeg -i intro.mp4 -i almost_done.mp4 -i outro.mp4 -filter_complex "[0:v] [0:a] [1:v] [1:a] [2:v] [2:a] concat=n=3:v=1:a=1 [v] [a]" -map "[v]" -map "[a]" -strict -2 merged.mp4 && \
-    cp merged.mp4 $4
+echo -e "\n\e[32mGenerating Images with guidolib\e[39m\n" && videogeneration $1 $2 && \
+    echo -e "\n\e[32mMerging images into video\e[39m\n" && ffmpeg -framerate 24 -i output%01d.png output.mp4 && \
+    echo -e "\n\e[32mAdding watermark on video\e[39m\n" && ffmpeg -i output.mp4 -i watermark_10a.png -filter_complex "overlay=0:0" output_watermarked.mp4 && \
+    echo -e "\n\e[32mDeleting generated images\e[39m\n" && rm -f output*.png && \
+    echo -e "\n\e[32mConverting raw audio midi solo to wav\e[39m\n" && sox -t f32 -r 48000 -c 1 audio.raw audio.wav && \
+    echo -e "\n\e[32mMerging solo wav and accompaniment mp3\e[39m\n" && ffmpeg -i audio.wav -i $3 -filter_complex amix=inputs=2:duration=longest final_audio.mp3 && \
+    echo -e "\n\e[32mAdding audio to generated video\e[39m\n" && ffmpeg -i output_watermarked.mp4 -i final_audio.mp3 -c copy almost_done.mp4 && \
+    echo -e "\n\e[32mMerging intro, generated video and outro\e[39m\n" && ffmpeg -i intro.mp4 -i almost_done.mp4 -i outro.mp4 -filter_complex "[0:v] [0:a] [1:v] [1:a] [2:v] [2:a] concat=n=3:v=1:a=1 [v] [a]" -map "[v]" -map "[a]" -strict -2 merged.mp4 && \
+    echo -e "\n\e[32mMoving finished file to desired output\e[39m\n" && cp merged.mp4 $4
