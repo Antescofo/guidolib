3a00100153758badce8a31038fb54217ab1a5a26
Victor Lenoir <victorlenoir@antescofo.com>
2019-11-18 15:56:00 +0100

    Update processing store

--- tools/generate_video.py
+++ tools/generate_video.py
@@ -27,7 +27,7 @@ if not DEBUG:
 BLACKLIST_PIECE_PK = []
 BLACKLIST_PIECE_PK += [478]  # 2 staffs issue, missing highlight
 BLACKLIST_PIECE_PK += [495, 498, 499, 500, 501, 502, 521]  # too fast oO
-BLACKLIST_PIECE_PK += [543] # ugly?
+BLACKLIST_PIECE_PK += [543, 1583] # ugly?
 BLACKLIST_PIECE_PK += [1544] # hella long
 
 def info_to_branch_item(video_title, piece_id):
