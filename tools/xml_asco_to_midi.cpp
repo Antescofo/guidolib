4cf9c0b8bb8b22bf36e9d23021c958b16db82302
Victor Lenoir <victorlenoir@antescofo.com>
2021-04-22 10:59:57 +0200

    add transpo parameter

--- tools/xml_asco_to_midi.cpp
+++ tools/xml_asco_to_midi.cpp
@@ -19,23 +19,25 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-  if (argc < 5) {
+  if (argc < 6) {
     cerr << "Usage: "
          << argv[0]
-         << " musicxml_file asco_file part_filter output_midi_file"
+         << " musicxml_file asco_file transpo part_filter output_midi_file"
          << endl;
     return 1;
   }
   string musicxml_file = argv[1];
   string asco_file = argv[2];
-  int part_filter = stoi(argv[3]);
-  string output_midi_file = argv[4];
+  int transpo = stoi(argv[3]);
+  int part_filter = stoi(argv[4]);
+  string output_midi_file = argv[5];
   
   std::cout << "Generate midi file from asco and xml" << std::endl;
   guidohttpd::makeApplication(argc, argv);
   guidohttpd::startEngine();
 
   MyMapCollector* collector = get_map_collector_from_xml_and_asco(musicxml_file, asco_file, part_filter);
+  collector->transpo -= transpo;
   generate_midi(collector, output_midi_file);
   return 0;
 }
